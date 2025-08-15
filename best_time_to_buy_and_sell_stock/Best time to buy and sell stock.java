1d4e724a0feb13e60ddfccb697ac74cabdba123f
root <root@87cbabbaf8ae>
2025-08-15 16:50:23 +0000

    Added solution for Unknown Problem - 2025-08-15 16:50:23

---
+++ best_time_to_buy_and_sell_stock/Best time to buy and sell stock.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = left + 1;
+        int maxProfit = 0;
+        int temp = 0 ;
+
+        while(right < prices.length){
+            
+            if(prices[left] < prices[right]){
+                temp = prices[right] - prices[left];
+                maxProfit = Math.max(temp , maxProfit);
+            }
+            else{
+                left = right;
+            }  
+            right++;
+            
+        }
+        return maxProfit;
+    }
+}
